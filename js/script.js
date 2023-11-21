9ec4222687105aaff9528599c5fbdee342c94a7c
muhammad idris assyafei <muhammadidrisasyafei@gmail.com>
2023-11-21 21:05:31 +0700

    first push

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    // Perform form validation here
-    var nama = document.getElementById('nama').value;
-    var email = document.getElementById('email').value;
-    var hobi = document.getElementById('hobi').value;
+    // Simple form validation
+    document.addEventListener('DOMContentLoaded', function () {
+        var form = document.querySelector('form');
 
-    if (nama === '' || email === '' || hobi === '') {
-        alert('Semua field harus diisi');
-        event.preventDefault(); // Prevent form submission if validation fails
-    }
-});
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            var name = form.querySelector('input[type="text"]').value.trim();
+            var email = form.querySelector('input[type="email"]').value.trim();
+            var message = form.querySelector('textarea').value.trim();
+
+            if (name === '' || email === '' || message === '') {
+                alert('field tidak boleh kosong ');
+                return;
+            }
+
+            // You can add an AJAX request here to send the form data to the server
+
+            // For now, show a simple confirmation message
+            alert('data di kirim suksess!');
+            form.reset();
+        });
+        var hubungiKamiLink = document.querySelector('header nav a[href="#hubungi-kami-section"]');
+        hubungiKamiLink.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            // Scroll smoothly to the "Hubungi Kami" section
+            document.getElementById('hubungi-kami-section').scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
